39122bf43c6adc2ff0f5f366c1719e4b6c7a6587
Alejandro3008 <ferralalejandro17@gmail.com>
2022-05-10 13:24:30 -0500

    SEO modified

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -36,7 +36,7 @@ export const Home = () => {
                 {data != null && data.results.map(el => (
                     <Card key={el.id} el={el} favorites={favorites} setFavorites={setFavorites}/>
                 ))}
-                {error && <Error image={'../images/rickError.png'} message={"Ohhh sh!t Morty the page can't load the information."} />}
+                {error && <Error image={'./images/rickError.png'} message={"Ohhh sh!t Morty the page can't load the information."} />}
                 <article className="w-100 mt-3 px-5 d-flex justify-content-end">
                 {data && <Button className="me-3 d-flex justify-content-center align-items-center" variant="light" onClick={handlePrev}>
                 <svg className="me-1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16"><path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/></svg>
