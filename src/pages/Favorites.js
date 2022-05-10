b87b11474393f135f5e422c436642dc20bb2686e
Alejandro3008 <ferralalejandro17@gmail.com>
2022-05-09 23:53:48 -0500

    img fixed

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -27,8 +27,8 @@ export const Favorites = () => {
                 {loading && <Loader/>}
                 {data != null && Array.isArray(data) && data.map(el => (<Card key={el.id} el={el} favorites={favorites} setFavorites={setFavorites}/>))}
                 {data != null && Array.isArray(data) === false && <Card key={data.id} el={data} favorites={favorites} setFavorites={setFavorites}/>}
-                {data === null && !noFav && <Error image={'../../public/images/rickError.png'} message={"You dont have a favorite characters"} />}
-                {error && noFav && <Error image={'../../public/images/rickError.png'} message={"Ohhh sh!t Morty the page can't load the information."} />}
+                {data === null && !noFav && <Error image={'./images/rickError.png'} message={"You dont have a favorite characters"} />}
+                {error && noFav && <Error image={'./images/rickError.png'} message={"Ohhh sh!t Morty the page can't load the information."} />}
             </Col>
         </>
     )
